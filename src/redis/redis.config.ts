a05a9eacb1466e78aca00b816d3a8c8b84cb51e1
Microwavekonijn <microwavekonijn@pm.me>
2022-05-24 22:34:28 +0200

    fix: redis config validation

--- src/redis/redis.config.ts
+++ src/redis/redis.config.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsUrl } from 'class-validator';
+import { IsUrl } from 'class-validator';
 import { ProcessEnv } from '@census-reworked/nestjs-utils';
 
 export class RedisConfig {
@@ -6,8 +6,6 @@ export class RedisConfig {
    * Connection string for Redis
    */
   @ProcessEnv('REDIS_URL')
-  @IsUrl()
-  @IsNotEmpty()
-  @IsUrl({})
+  @IsUrl({ protocols: ['redis'] })
   url = 'redis://127.0.0.1:6379';
 }
